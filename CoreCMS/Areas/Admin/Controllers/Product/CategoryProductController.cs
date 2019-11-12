9b7ad5f5173a7a0347d8640c579131b42a051bac
pham danh phong <vanluutinh@hotmail.com>
2019-11-12 22:31:39 +0700

    up code

---
+++ CoreCMS/Areas/Admin/Controllers/Product/CategoryProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCMS.Areas.Admin.Controllers.Product
+{
+    public class CategoryProductController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreCMS/Areas/Admin/Controllers/Product/ItemProduct/ItemProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCMS.Areas.Admin.Controllers.Product.ItemProduct
+{
+    public class ItemProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
